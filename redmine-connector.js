a1ae77f1e557a5f654011fbefd2db960069e3db0
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-05-07 21:56:16 +0200

    api declaration

---
+++ redmine-connector.js
@@ -0,0 +1,10 @@
+async function redmineLogin(username, password) {
+
+}
+
+async function getExport(username, password, year, month) {
+
+}
+
+modules.export.redmineLogin = redmineLogin;
+modules.export.getExport = getExport;
