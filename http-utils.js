b8bbdc53a47bc4c1ce5d04222047f2e83cc6a913
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-05-08 20:58:20 +0200

    dev

--- http-utils.js
+++ http-utils.js
@@ -1,75 +1,64 @@
 var rest = require('restler');
 
 function get(url, queryData, headers) {
-    return new Promise(
-        function(resolve, reject) {
-            queryData = queryData ? (jsonToQueryString(queryData)) : "";
-            rest.get(url + queryData).on('complete', function(result) {
-                if (result instanceof Error) reject(result);
-                else resolve(result);
-            });
-        }
-    );
+	return new Promise(
+			queryData = queryData ? (jsonToQueryString(queryData)) : "";
+				if (result instanceof Error) reject(result);
+				else resolve(result);
+			});
+		}
+	);
 }
 
 function post(url, queryData, data, headers, isMultipart) {
-    return new Promise(
-        function(resolve, reject) {
-            queryData = queryData ? (jsonToQueryString(queryData)) : "";
-            rest.post(url + queryData).on('complete', function(result) {
-                if (result instanceof Error) reject(result);
-                else resolve(result);
-            });
-        }
-    );
+	return new Promise(
+			queryData = queryData ? (jsonToQueryString(queryData)) : "";
+				if (result instanceof Error) reject(result);
+				else resolve(result);
+			});
+		}
+	);
 }
 
 function put(url, queryData, data, headers, isMultipart) {
-    return new Promise(
-        function(resolve, reject) {
-            queryData = queryData ? (jsonToQueryString(queryData)) : "";
-            rest.put(url + queryData).on('complete', function(result) {
-                if (result instanceof Error) reject(result);
-                else resolve(result);
-            });
-        }
-    );
+	return new Promise(
+			queryData = queryData ? (jsonToQueryString(queryData)) : "";
+				if (result instanceof Error) reject(result);
+				else resolve(result);
+			});
+		}
+	);
 }
 
-function delete(url, queryData, headers) {
-    return new Promise(
-        function(resolve, reject) {
-            queryData = queryData ? (jsonToQueryString(queryData)) : "";
-            rest.delete(url + queryData).on('complete', function(result) {
-                if (result instanceof Error) reject(result);
-                else resolve(result);
-            });
-        }
-    );
+function deleteFn(url, queryData, headers) {
+	return new Promise(
+			queryData = queryData ? (jsonToQueryString(queryData)) : "";
+				if (result instanceof Error) reject(result);
+				else resolve(result);
+			});
+		}
+	);
 }
 
 function option(url, queryData, headers) {
-    return new Promise(
-        function(resolve, reject) {
-            queryData = queryData ? (jsonToQueryString(queryData)) : "";
-            rest.option(url + queryData).on('complete', function(result) {
-                if (result instanceof Error) reject(result);
-                else resolve(result);
-            });
-        }
-    );
+	return new Promise(
+			queryData = queryData ? (jsonToQueryString(queryData)) : "";
+				if (result instanceof Error) reject(result);
+				else resolve(result);
+			});
+		}
+	);
 }
 
 function parseHeaders(headers) {
-    headers = headers || getHeadersObject();
+	headers = headers || getHeadersObject();
 }
 
 function jsonToQueryString(json) {
-    return '?' +
-        Object.keys(json).map(function(key) {
-            return encodeURIComponent(key) + '=' +
-                encodeURIComponent(json[key]);
-        }).join('&');
+	return '?' +
+			return encodeURIComponent(key) + '=' +
+				encodeURIComponent(json[key]);
+		}).join('&');
 }
 
 function getHeadersObject() {
@@ -80,10 +69,10 @@ function getHeadersObject() {
 }
 
 module.exports = {
-    get: get,
-    post: post,
-    put: put,
-    delete: delete,
-    option: option,
-    getHeadersObject: getHeadersObject
+	get: get,
+	post: post,
+	put: put,
+	delete: deleteFn,
+	option: option,
+	getHeadersObject: getHeadersObject
 };
